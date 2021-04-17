d023281d1c6b29dd37413cd7ba57c71e310529aa
CarlosEgas <carlosaquiles81@gmail.com>
2021-04-16 22:50:27 -0500

    primera entrega

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ce-root',
   templateUrl: './app.component.html',
+  //template: '<h1>Hola Carlos Egas</h1>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'restaurant';
+  private dish = 'carapulca';
+
+  taste = false;
 }
---
+++ src/app/dish/dish.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ce-dish',
+  templateUrl: './dish.component.html',
+  styleUrls: ['./dish.component.scss']
+})
+export class DishComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
